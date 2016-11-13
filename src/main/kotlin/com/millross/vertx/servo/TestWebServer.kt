8af3e9d7db53fe1478f2983d634b966ca107b1bf
Jeremy Prime <jeremy.prime@millross-consultants.com>
2016-11-13 15:35:22 +0000

    Expose metrics via JMX via servo

--- src/main/kotlin/com/millross/vertx/servo/TestWebServer.kt
+++ src/main/kotlin/com/millross/vertx/servo/TestWebServer.kt
@@ -1,26 +1,36 @@
 package com.millross.vertx.servo
 
+import com.netflix.servo.annotations.DataSourceType.COUNTER
+import com.netflix.servo.annotations.Monitor
+import com.netflix.servo.monitor.Monitors
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Simple test webserver verticle to try and collect some trivial metrics via servo
  */
 class TestWebServer : AbstractVerticle() {
 
+    // Servo monitoring of the invocation count
+    @Monitor(name="invocation.count", type=COUNTER)
+    private val invocationCount = AtomicInteger(0)
+
     override fun start(startFuture: Future<Void>?) {
         super.start(startFuture)
         val router = Router.router(vertx)
         router.get("/testMetric").handler { handleMetricTest(it) }
         router.get("/").handler { index(it) }
+        Monitors.registerObject("TestWebServer", this)
         vertx.createHttpServer()
                 .requestHandler { router.accept(it) }
                 .listen(8080)
     }
 
     fun handleMetricTest(rc: RoutingContext) {
+        invocationCount.incrementAndGet()
         rc.response().end("Metric triggered")
     }
 
